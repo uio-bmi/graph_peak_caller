9ce345706ce21bd359e871aa5fa8bd207b98a1c2
Ivar Grytten <ivar.grytten@gmail.com>
2017-04-21 14:28:17 +0200

    testing

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -3,8 +3,10 @@ import unittest
 class TestSomething(unittest.TestCase):
 
 	def test_some_method(self):
+		self.assertEqual(2 + 2, 3)
+	
+	def test_something_correct(self):
 		self.assertEqual(2 + 2, 4)
-		self.assertTrue(True)
 	
 if __name__ == "__main__":
     unittest.main()
