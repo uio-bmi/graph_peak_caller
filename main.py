f32e71741f9504e6d5c630c807afd7f9db8ef1e9
Knut Rand <knutdrand@gmail.com>
2017-08-21 15:04:26 +0200

    starting pipeline

--- main.py
+++ main.py
@@ -2,7 +2,31 @@ import graph_peak_caller.vg as vg
 import offsetbasedgraph
 import json
 from graph_peak_caller.pileup import Pileup
+from graph_peak_caller.get_lambda_track import BackgroundTrack, ControlInfo
 
+
+class Pipeline(object):
+
+    def __init__(self, graph_file_name, sample_file_name, control_file_name=None, chromosome=None):
+        self.graph_file_name = graph_file_name
+        self.sample_file_name = sample_file_name
+        self.control_file_name = control_file_name if control_file_name is not None else sample_file_name
+        self.has_control = control_file_name is not None
+        self.create_graphs()
+        self.chromosome = chromosome
+
+    def 
+
+    def create_graphs(self):
+        self.vg_graph = vg.Graph.create_from_file(self.graph_file_name)
+        self.ob_graph = vg_graph.get_offset_based_graph()
+
+    def create_control(self):
+        bg_track = BackgroundTrack(self.ob_graph, control_file_name, self.d, 1000, 10000, 
+        f = open()
+
+        jsons = (json.loads(line) for line in f.readlines())
+        alignments = [vg.Alignment.from_json(json_dict) for json_dict in jsons]        
 
 if __name__ == "__main__":
     print("Creating vg graph")
