05b90395934a04a7f8f76dae97235373cb8d022b
Ivar Grytten <ivar.grytten@gmail.com>
2017-10-30 15:48:11 +0100

    Init snarls

---
+++ graph_peak_caller/snarls.py
@@ -0,0 +1,15 @@
+from offsetbasedgraph import Graph
+
+class Snarl(Graph):
+
+    def __init__(blocks, edges):
+        pass
+
+    @classmethod
+    def create_from_start_and_end(cls, start, end, parent_graph):
+        pass
+
+    def length(self):
+        pass
+
+    
