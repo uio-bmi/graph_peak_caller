36f0020063bd5fedc6dd35396d5a52bd23a917da
Knut Rand <knutdrand@gmail.com>
2017-11-03 17:11:13 +0100

    push

--- graph_peak_caller/eventsorter.py
+++ graph_peak_caller/eventsorter.py
@@ -3,17 +3,13 @@ import numpy as np
 
 class EventSorter(object):
     def __init__(self, index_lists, values_list, names=None):
-        print("+++", index_lists)
-        print("###", values_list)
         if names is not None:
             [setattr(self, name.upper(), i) for i, name in enumerate(names)]
 
         n_types = len(index_lists)
         coded_index_list = [np.array(index_list)*n_types + r
                             for r, index_list in enumerate(index_lists)]
-        print("LLL", coded_index_list)
         coded_indices = np.concatenate(coded_index_list)
-        print("SSS", coded_indices)
         sorted_args = np.argsort(coded_indices)
         coded_indices = coded_indices[sorted_args]
         self.indices = coded_indices//n_types
