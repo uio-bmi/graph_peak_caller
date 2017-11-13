3ee91db6f2d169a55e09bd6deb5ecde33b1cab7d
Knut Rand <knutdrand@gmail.com>
2017-11-13 13:35:42 +0100

    extension /2

--- graph_peak_caller/linearsnarls.py
+++ graph_peak_caller/linearsnarls.py
@@ -18,11 +18,14 @@ def create_control(linear_map_name, reads, extension_sizes, fragment_length):
     logging.info("Average control value: %.4f (sum of pileup: %d, linear size: %d)" % (average_value, mapped_reads.n_basepairs_covered(), linear_size))
     max_pileup = LinearPileup([0], [average_value])
 
-    for extension in extension_sizes:
+    for tmp_extension in extension_sizes:
+        extension = tmp_extension//2
+        print("#########", extension)
         extended_reads = mapped_reads.extend(extension)
         linear_pileup = LinearPileup.create_from_starts_and_ends(
                 extended_reads.starts, extended_reads.ends)
         linear_pileup /= (extension*2/fragment_length)
+        print(np.max(linear_pileup.values))
         max_pileup.maximum(linear_pileup)
     # max_pileup.threshold(average_value)
     valued_indexes = max_pileup.to_valued_indexes(linear_map)
