af02e8c5f25c8ab429c9ca58b2249f80fa8f3931
Ivar Grytten <ivar.grytten@gmail.com>
2017-11-17 17:10:31 +0100

    Extracted pre-callpeaks stuff from run to separate method

--- graph_peak_caller/callpeaks.py
+++ graph_peak_caller/callpeaks.py
@@ -193,6 +193,10 @@ class CallPeaks(object):
         self.cutoff = value
 
     def run(self, out_file="final_peaks.bed"):
+        self.run_pre_call_peaks_steps()
+        self.call_peaks()
+
+    def run_pre_callpeaks_steps(self):
         self.create_graph()
         self.preprocess()
         if self.info is None:
@@ -202,7 +206,6 @@ class CallPeaks(object):
         self.create_control(True)
         self.scale_tracks()
         self.get_score()
-        self.call_peaks()
 
     def preprocess(self):
         self.info.n_control_reads = 0
@@ -256,6 +259,7 @@ class CallPeaks(object):
         ratio = self.info.n_sample_reads/self.info.n_control_reads
 
         if self.info.n_sample_reads == self.info.n_control_reads:
+            logging.info("Not scaling any tracks because of same amount of reads")
             return
 
         if ratio > 1:
@@ -274,7 +278,6 @@ class CallPeaks(object):
                 self._control_pileup.to_bed_graph(self._control_track)
 
     def find_info(self):
-        genome_size = 0
         sizes = (block.length() for block in self.ob_graph.blocks.values())
 
         self.genome_size = sum(sizes)
@@ -291,14 +294,10 @@ class CallPeaks(object):
     def create_control(self, save_to_file=True):
         logging.info("Creating control track using linear map %s" % self.linear_map)
 
-        extensions = [self.info.fragment_length, 1000, 10000] if self.has_control else [10000]
+        #extensions = [self.info.fragment_length, 1000, 10000] if self.has_control else [10000]
+        extensions = [10000] #if self.has_control else [10000]
         control_pileup = linearsnarls.create_control(self.linear_map,  self.control_intervals,
                                                      extensions, self.info.fragment_length)
-
-        #control_track = ControlTrack(
-        #    self.ob_graph, self.control_intervals,
-        #    self.info.fragment_length, extensions)
-        #pileup = control_track.get_control_track(self._sample_pileup.mean())
 
         logging.info("Number of control reads: %d" % self.info.n_control_reads)
 
