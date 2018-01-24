20efade8e87b4fe61fb704eff26417f50e919105
Ivar Grytten <ivar.grytten@gmail.com>
2018-01-24 11:18:20 +0100

    Bugfix

--- graph_peak_caller/multiplegraphscallpeaks.py
+++ graph_peak_caller/multiplegraphscallpeaks.py
@@ -89,4 +89,4 @@ class MultipleGraphsCallpeaks:
             caller.p_values_pileup = DensePileup.from_sparse_files(
                 ob_graph, self._base_name + name + "_" + "pvalues")
             caller.get_q_values()
-            caller.call_peaks_from_q_values(experiment_info=info)
+            caller.call_peaks_from_q_values(experiment_info=info, config=self._config)
