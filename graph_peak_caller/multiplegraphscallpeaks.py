c01053414ad7e7ec922348dd1ba660d9fdba1515
Ivar Grytten <ivar.grytten@gmail.com>
2018-01-25 13:12:55 +0100

    Div bugfixing and assertion fixing for p values to q values

--- graph_peak_caller/multiplegraphscallpeaks.py
+++ graph_peak_caller/multiplegraphscallpeaks.py
@@ -89,3 +89,4 @@ class MultipleGraphsCallpeaks:
                 ob_graph, self._base_name + name + "_" + "pvalues")
             caller.get_q_values()
             caller.call_peaks_from_q_values(experiment_info=info, config=self._config)
+            caller.save_max_path_sequences_to_fasta_file("sequences.fasta", self.sequence_retrievers[i])
