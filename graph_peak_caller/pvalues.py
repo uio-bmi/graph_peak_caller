e963daf0da31b73a93ca70c0205909a962d367cc
Ivar Grytten <ivar.grytten@gmail.com>
2018-01-31 11:42:44 +0100

    cleanup

--- graph_peak_caller/pvalues.py
+++ graph_peak_caller/pvalues.py
@@ -69,7 +69,6 @@ class PToQValuesMapper:
         return cls(counter)
 
     def get_p_to_q_values(self):
-        logging.info("Number of zero pvalues: %d" % self._counter[0])
         logging.info("Mapping p to q values")
         p_to_q_values = {}
         rank = 1
