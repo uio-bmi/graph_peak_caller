955c15c98722bcefc6472cef331eabfa0af035d0
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-01 15:35:30 +0100

    Added matplolib dependency

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(name='graph_peak_caller',
       packages=['graph_peak_caller'],
       zip_safe=False,
       install_requires=['pymysql', 'numpy', 'filecache', 'scipy', 'pybedtools', 'pyBigWig',
-                        'memory_profiler', 'python-coveralls'],
+                        'memory_profiler', 'python-coveralls', 'matplotlib'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ]
