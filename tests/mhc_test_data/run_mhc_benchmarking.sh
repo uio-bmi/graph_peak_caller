fb6a4a818d0886085421422ae7904ff06a66a82d
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-01 15:30:25 +0100

    Added mhc benchmarking to travis

---
+++ tests/mhc_test_data/run_mhc_benchmarking.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+python3 ../../graph_peak_caller.py create_ob_numpy_graph haplo1kg50-mhc.json graph
+python3 ../../graph_peak_caller.py create_ob_graph haplo1kg50-mhc.json graph.obg
+
+# Create snarls
+python3 ../../graph_peak_caller.py create_linear_map graph.obg haplo1kg50-mhc.snarls linear_map
+
+# Call peaks
+python3 ../../graph_peak_caller.py callpeaks_with_numpy_graph graph haplo1kg50-mhc.vg linear_map macs_remapped_mhc.json macs_remapped_mhc.json False test_ 135 36
+
+# Motif enrichment
+python3 ../../graph_peak_caller.py plot_motif_enrichment test_sequences.fasta macs_sequences.fasta MA0139.1.meme plot.png
+
+ls plot.png
