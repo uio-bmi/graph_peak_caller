a7294d3999f1cb270fe2048176635ce9766c773f
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-02 21:56:59 +0100

    Dependency fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='graph_peak_caller',
       zip_safe=False,
       install_requires=['pymysql', 'numpy', 'filecache', 'scipy', 'pybedtools', 'pyBigWig',
                         'memory_profiler', 'python-coveralls', 'matplotlib', 'codecov', 'pytest-cov',
-                        'biopython'],
+                        'biopython', 'pyfaidx'],
       classifiers=[
             'Programming Language :: Python :: 3'
       ],
