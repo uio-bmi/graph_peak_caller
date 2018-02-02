6fe3a405440fd39d5ac4d83acac2b7aa8286076a
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-02 16:35:31 +0100

    Bugfix

--- tests/mhc_test_data/run_mhc_benchmarking.sh
+++ tests/mhc_test_data/run_mhc_benchmarking.sh
@@ -7,7 +7,7 @@ graph_peak_caller create_ob_graph haplo1kg50-mhc.json graph.obg
 graph_peak_caller create_linear_map graph.obg haplo1kg50-mhc.snarls linear_map
 
 # Call peaks
-graph_peak_callery callpeaks_with_numpy_graph graph haplo1kg50-mhc.vg linear_map reads.json reads.json False test_ 135 36
+graph_peak_caller callpeaks_with_numpy_graph graph haplo1kg50-mhc.vg linear_map reads.json reads.json False test_ 135 36
 
 # Motif enrichment
 #graph_peak_caller plot_motif_enrichment test_sequences.fasta macs_sequences.fasta MA0139.1.meme plot.png
