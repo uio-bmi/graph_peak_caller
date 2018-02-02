eec70a3302a0f26f9b0790153620f691d3be2753
Ivar Grytten <ivar.grytten@gmail.com>
2018-02-02 10:37:31 +0100

    Removed old interface file

--- tests/mhc_test_data/run_mhc_benchmarking.sh
+++ tests/mhc_test_data/run_mhc_benchmarking.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
-python3 ../../graph_peak_caller.py create_ob_numpy_graph haplo1kg50-mhc.json graph
-python3 ../../graph_peak_caller.py create_ob_graph haplo1kg50-mhc.json graph.obg
+graph_peak_caller create_ob_numpy_graph haplo1kg50-mhc.json graph
+graph_peak_caller create_ob_graph haplo1kg50-mhc.json graph.obg
 
 # Create snarls
-python3 ../../graph_peak_caller.py create_linear_map graph.obg haplo1kg50-mhc.snarls linear_map
+graph_peak_caller create_linear_map graph.obg haplo1kg50-mhc.snarls linear_map
 
 # Call peaks
-python3 ../../graph_peak_caller.py callpeaks_with_numpy_graph graph haplo1kg50-mhc.vg linear_map reads.json reads.json False test_ 135 36
+graph_peak_callery callpeaks_with_numpy_graph graph haplo1kg50-mhc.vg linear_map reads.json reads.json False test_ 135 36
 
 # Motif enrichment
-#python3 ../../graph_peak_caller.py plot_motif_enrichment test_sequences.fasta macs_sequences.fasta MA0139.1.meme plot.png
+graph_peak_caller plot_motif_enrichment test_sequences.fasta macs_sequences.fasta MA0139.1.meme plot.png
 
 #ls plot.png
