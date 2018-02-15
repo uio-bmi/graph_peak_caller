f7b6b52fe97c85cf1db5f10f8110cb3e868a3fc8
Knut Rand <knutdrand@gmail.com>
2018-02-15 17:56:05 +0100

    reverted run_mhc_benchmarking

--- tests/mhc_test_data/run_mhc_benchmarking.sh
+++ tests/mhc_test_data/run_mhc_benchmarking.sh
@@ -6,14 +6,14 @@ graph_peak_caller create_ob_graph haplo1kg50-mhc.json graph.nobg
 graph_peak_caller create_linear_map graph.nobg haplo1kg50-mhc.snarls linear_map
 
 # Call peaks
-graph_peak_caller callpeaks graph.nobg haplo1kg50-mhc.vg linear_map reads.json reads.json False sparsetest_ 135 36
+graph_peak_caller callpeaks graph.nobg haplo1kg50-mhc.vg linear_map reads.json reads.json False test_ 135 36
 
 # Run fimo
-fimo -oc fimo_sparsetest_sequences MA0139.1.meme sparsetest_sequences.fasta
+fimo -oc fimo_test_sequences MA0139.1.meme test_sequences.fasta
 fimo -oc fimo_macs_sequences MA0139.1.meme macs_sequences_mhc.fasta
 
 # Motif enrichment
-graph_peak_caller plot_motif_enrichment sparsetest_sequences.fasta macs_sequences_mhc.fasta MA0139.1.meme plot.png True
+graph_peak_caller plot_motif_enrichment test_sequences.fasta macs_sequences_mhc.fasta MA0139.1.meme plot.png True
 
 # graph_peak_caller analyse_peaks graph.obg haplo1kg50-mhc.json macs_sequences_mhc.fasta test_sequences.fasta fimo_test_sequences/fimo.txt fimo_macs_sequences_mhc/fimo.txt chr6 28510119 33480577
 
