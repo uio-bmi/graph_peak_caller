fe6476c0ad72108882a76909256d5e0eadbd0bbb
Knut Rand <knutdrand@gmail.com>
2018-02-28 17:15:21 +0100

    fixed touched nodes

--- tests/test_sparseholescleaning.py
+++ tests/test_sparseholescleaning.py
@@ -2,8 +2,10 @@ import numpy as np
 import offsetbasedgraph as obg
 from graph_peak_caller.sparseholecleaner import HolesCleaner
 from graph_peak_caller.sparsediffs import SparseValues
+import pytest
 
 
+@pytest.fixture
 def complicated_graph():
     nodes = {i: obg.Block(2) for i in range(1, 11)}
     edges = {1: [2, 3],
@@ -17,6 +19,7 @@ def complicated_graph():
     return obg.GraphWithReversals(nodes, edges)
 
 
+@pytest.fixture
 def complicated_offset():
     nodes = {i: obg.Block(2) for i in range(101, 111)}
     edges = {101: [102, 103],
@@ -27,6 +30,17 @@ def complicated_offset():
              106: [107],
              107: [108, 109],
              109: [110]}
+    return obg.GraphWithReversals(nodes, edges)
+
+
+@pytest.fixture
+def small_graph():
+    nodes = {i: obg.Block(10) for i in range(101, 107)}
+    edges = {101: [102],
+             102: [103, 104],
+             103: [105],
+             104: [105],
+             105: [106]}
     return obg.GraphWithReversals(nodes, edges)
 
 
@@ -65,29 +79,25 @@ def test_long_hole():
     assert holes == pileup
 
 
-def test_complicated():
-    graph = complicated_graph()
+def test_complicated(complicated_graph):
+    # graph = complicated_graph()
     #                      
     pileup = SparseValues(
         #         1  1  2--3  4  5---6   7   8
         np.array([0, 1, 2, 4, 6, 8, 10, 12, 14], dtype="int"),
         np.array([1, 0, 1, 0, 1, 0,  1, 0,  1], dtype="bool"))
-    holes = HolesCleaner(graph, pileup, 3).run()
+    holes = HolesCleaner(complicated_graph, pileup, 3).run()
     true = SparseValues([0, 8, 10], [1, 0, 1])
-    print(holes)
     assert holes == true
 
 
-def test_offset():
-    graph = complicated_offset()
-    #                      
+def test_offset(complicated_offset):
     pileup = SparseValues(
         #         1  1  2--3  4  5---6   7   8
         np.array([0, 1, 2, 4, 6, 8, 10, 12, 14], dtype="int"),
         np.array([1, 0, 1, 0, 1, 0,  1, 0,  1], dtype="bool"))
-    holes = HolesCleaner(graph, pileup, 3).run()
+    holes = HolesCleaner(complicated_offset, pileup, 3).run()
     true = SparseValues([0, 8, 10], [1, 0, 1])
-    print(holes)
     assert holes == true
 
 
@@ -106,8 +116,25 @@ def test_internals():
     assert cleaned == true
 
 
+def test_touched_edge(small_graph):
+    touched_nodes = set([101, 103, 106])
+    pileup = SparseValues([0, 4, 22, 28, 56],
+                          [1, 0,  1,  0,  1])
+    cleaned = HolesCleaner(small_graph, pileup, 4, touched_nodes).run()
+    assert cleaned == pileup
+
+
+# @pytest.mark.skip
+def test_non_touched_mid(small_graph):
+    touched_nodes = set([101, 102, 103, 104, 105, 106])
+    pileup = SparseValues([0, 4, 22, 28, 56],
+                          [1, 0,  1,  0,  1])
+    cleaned = HolesCleaner(small_graph, pileup, 20, touched_nodes).run()
+    true = SparseValues([0, 30, 40], [1, 0, 1])
+    assert cleaned == true
+
+
 if __name__ == "__main__":
     # test_holes_cleaner()
     # test_end_hole()
     test_internals()
-    
