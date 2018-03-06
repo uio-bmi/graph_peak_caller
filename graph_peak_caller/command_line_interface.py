60892824393eb8c7fc3d25c7c77e014dcdf879f3
Ivar Grytten <ivar.grytten@gmail.com>
2018-03-06 18:50:37 +0100

    Manifest file for including subirs

--- graph_peak_caller/command_line_interface.py
+++ graph_peak_caller/command_line_interface.py
@@ -32,7 +32,7 @@ def main():
     create_argument_parser()
 
 def version(args):
-    print("Graph Peak Caller v1.0.1")
+    print("Graph Peak Caller v1.0.2")
 
 def concatenate_sequence_files(args):
     chromosomes = args.chromosomes.split(",")
