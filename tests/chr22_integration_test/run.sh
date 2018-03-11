c14079be3003e9d9fa201e7adba323c37b011191
Ivar Grytten <ivar.grytten@gmail.com>
2018-03-11 22:36:35 +0100

    UPdated chr22 integration test

--- tests/chr22_integration_test/run.sh
+++ tests/chr22_integration_test/run.sh
@@ -1,7 +1,8 @@
 
 #macs2 callpeak -g 51304566 -t linear_alignments_chr22.bam -n macs -m 5 50 
 
-graph_peak_caller callpeaks 22.nobg 22.json linear_map_22 reads_moved_to_graph_22.intervalcollection reads_moved_to_graph_22.intervalcollection False 22_ 146 35
+graph_peak_caller callpeaks -g 22.nobg -m linear_map_22 -s reads_moved_to_graph_22.intervalcollection -n 22_ -f 146 -r 35
+
 
 fimo -oc fimo_macs_chr22 motif.meme macs_sequences_chr22.fasta
 fimo -oc fimo_graph_chr22 motif.meme 22_sequences.fasta
