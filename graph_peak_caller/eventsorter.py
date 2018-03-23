79d0cce57bda54f8be3c484cab0808eb1f464952
Knut Rand <knutdrand@gmail.com>
2018-03-23 17:51:08 +0100

    removed unused eventsort code

--- graph_peak_caller/eventsorter.py
+++ graph_peak_caller/eventsorter.py
@@ -2,32 +2,6 @@ import numpy as np
 from operator import itemgetter
 
 
-class DiscreteEventSorter(object):
-    def __init__(self, index_lists, values_list=None, names=None):
-        if names is not None:
-            [setattr(self, name.upper(), i) for i, name in enumerate(names)]
-
-        n_types = len(index_lists)
-        coded_index_list = [np.array(index_list)*n_types + r
-                            for r, index_list in enumerate(index_lists)]
-
-        coded_indices = np.concatenate(coded_index_list)
-        sorted_args = np.argsort(coded_indices)
-        coded_indices = coded_indices[sorted_args]
-        self.indices = coded_indices // n_types
-        self.codes = coded_indices % n_types
-        if values_list is not None:
-            self.values = np.concatenate(values_list)[sorted_args]
-
-    def __iter__(self):
-        return zip(self.indices, self.codes, self.values)
-
-    def pileup(self):
-        codes = self.codes*2-1
-        values = np.cumsum(codes)
-        return self.indices, values
-
-
 class EventSorter:
 
     def np_init(self, index_lists, values_lists):
@@ -61,7 +35,6 @@ class EventSorter:
 
 class EventSort(object):
     def __init__(self, index_lists, codes, names=None):
-        #print("Running eventnsorter for indexes %s with codes %s" % (index_lists, codes))
         if names is not None:
             [setattr(self, name.upper(), i) for i, name in enumerate(names)]
         self.tuples = []
@@ -78,25 +51,5 @@ class EventSort(object):
         self.indices[1:] = indices[new_values]
         self.indices[0] = indices[0]
 
-        self.values[1:] = values[new_values]
-        self.values[0] = values[0]
-
-class DiscreteEventSort(object):
-    def __init__(self, index_lists, codes, names=None):
-        if names is not None:
-            [setattr(self, name.upper(), i) for i, name in enumerate(names)]
-        self.tuples = []
-        for index_list, code in zip(index_lists, codes):
-            self.tuples.extend([(idx, code) for idx in index_list])
-        self.tuples.sort(key=itemgetter(0, 1))
-        values = np.array([t[1] for t in self.tuples])
-        indices = np.array([t[0] for t in self.tuples])
-        values = np.cumsum(values)
-        new_values = np.nonzero(np.diff(values))[0] + 1
-        self.values = np.empty(new_values.size+1)
-        self.indices = np.empty(new_values.size+1, dtype="int")
-
-        self.indices[1:] = indices[new_values]
-        self.indices[0] = indices[0]
         self.values[1:] = values[new_values]
         self.values[0] = values[0]
