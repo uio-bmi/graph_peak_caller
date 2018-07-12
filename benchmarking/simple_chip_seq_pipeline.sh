2d029acccf59a53cc3ccc1802826c90b3bfe11e1
Ivar Grytten <ivar.grytten@gmail.com>
2018-07-12 11:23:13 +0200

    Jenkins dev

--- benchmarking/simple_chip_seq_pipeline.sh
+++ benchmarking/simple_chip_seq_pipeline.sh
@@ -155,17 +155,17 @@ fi
 for chromosome in $(echo $chromosomes | tr "," "\n")
 do 
     echo "Projecting alignments for chrom $chromosome"
-    graph_peak_caller project_vg_alignments -g $graph_dir/$chromosome.nobg filtered_low_qual_reads_removed_$chromosome.json $graph_dir/${chromosome}_linear_pathv2.interval $chromosome projected_alignments_$chromosome.bed &
+    #graph_peak_caller project_vg_alignments -g $graph_dir/$chromosome.nobg filtered_low_qual_reads_removed_$chromosome.json $graph_dir/${chromosome}_linear_pathv2.interval $chromosome projected_alignments_$chromosome.bed &
 done
 
-wait  # Wait for all projections
+#wait  # Wait for all projections
 cat projected_alignments_*.bed > projected_alignments.bed
 
 # Run MACS2 with projected alignments
 echo "Running macs2 with projected alignments."
 macs2 callpeak --nomodel --extsize $fragment_length -g $genome_size -t projected_alignments.bed -n macs_projected_alignments > macs_output_projected_alignments.txt 2>&1
 
-
+echo "Counting unique reads"
 # Count unique reads in filtered files
 if [ ! -f count_unique_reads_output.txt ]; then
     graph_peak_caller count_unique_reads $chromosomes $graph_dir/ filtered_low_qual_reads_removed_ > count_unique_reads_output.txt 2>&1
