04046bcf04dd6adb837cb54de1600b818fbe8adf
root <ivarknut@server>
2018-08-20 14:46:44 +0200

    Div old changes

--- graph_peak_caller/analysis/analysis_interface.py
+++ graph_peak_caller/analysis/analysis_interface.py
@@ -100,16 +100,16 @@ def check_haplotype(args):
     main = Main.from_name(name, args.fasta_file, args.chrom)
 
     motif_paths = obg.IntervalCollection.from_file(
-        args.result_folder+args.chrom+"_motif_paths.intervalcollection", True)
+        args.result_folder+args.chrom+"_" + args.interval_name + ".intervalcollection", True)
     graph = obg.Graph.from_file(args.data_folder+args.chrom+".nobg")
-    # alignment_collection = AlignmentCollection.from_file(
-    #    args.result_folder + args.chrom + "_alignments.pickle", graph)
-    # peaks_dict = {i: alignment_collection.get_alignments_on_interval(interval).values()
-    #               for i, interval in enumerate(motif_paths)}
-    # IntervalDict(peaks_dict).to_file(args.result_folder + args.chrom + "_motif_reads.intervaldict")
-    peaks_dict = IntervalDict.from_file(args.result_folder + args.chrom + "_motif_reads.intervaldict").intervals
+    alignment_collection = AlignmentCollection.from_file(
+       args.result_folder + args.chrom + "_alignments.pickle", graph)
+    peaks_dict = {i: alignment_collection.get_alignments_on_interval(interval).values()
+                  for i, interval in enumerate(motif_paths)}
+    IntervalDict(peaks_dict).to_file(args.result_folder + args.chrom + args.interval_name + "_reads.intervaldict")
+    # peaks_dict = IntervalDict.from_file(args.result_folder + args.chrom + "_motif_reads.intervaldict").intervals
     result_dict = main.run_peak_set(peaks_dict)
-    with open(args.result_folder + args.chrom + "_motif_paths.setsummary", "w") as f:
+    with open(args.result_folder + args.chrom + "_"+ args.interval_name + ".setsummary", "w") as f:
         for k, v in result_dict.items():
             print("%s, %s" % (k, summarize_haplotypes(v)))
             f.write("%s, %s\n" % (k, summarize_haplotypes(v)))
@@ -118,7 +118,7 @@ def check_haplotype(args):
              for result in results)
     # motif_paths = list(sorted(motif_paths, key=lambda x: x.region_paths[0]))
     # haplotypes = main.run_peaks(motif_paths)
-    with open(args.result_folder + args.chrom + "_motif_paths.setcoverage", "w") as f:
+    with open(args.result_folder + args.chrom + "_" + args.interval_name + ".setcoverage", "w") as f:
         for line in lines:
             f.write(line+"\n")
         # for haplotype in haplotypes:
