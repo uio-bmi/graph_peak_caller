8c96f76d3b75743b542bc295e879f3c3a0d9d7b8
Ivar Grytten <ivar.grytten@gmail.com>
2018-09-16 23:46:38 +0200

    Mhc bug

--- tests/mhc_test_data/run_mhc_benchmarking.sh
+++ tests/mhc_test_data/run_mhc_benchmarking.sh
@@ -6,8 +6,8 @@ graph_peak_caller create_ob_graph -o graph.nobg haplo1kg50-mhc.json
 graph_peak_caller callpeaks -g graph.nobg --sample reads.json -n test_ -f 135 -r 36
 
 # Run fimo
-fimo -oc fimo_test_sequences MA0139.1.meme test_sequences.fasta
-fimo -oc fimo_macs_sequences MA0139.1.meme macs_sequences_mhc.fasta
+#fimo -oc fimo_test_sequences MA0139.1.meme test_sequences.fasta
+#fimo -oc fimo_macs_sequences MA0139.1.meme macs_sequences_mhc.fasta
 
 # Motif enrichment
 #graph_peak_caller plot_motif_enrichment test_sequences.fasta macs_sequences_mhc.fasta MA0139.1.meme plot.png True CTCF None
